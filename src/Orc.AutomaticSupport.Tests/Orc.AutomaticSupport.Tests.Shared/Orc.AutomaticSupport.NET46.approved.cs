98463e81e21de2992115b8e75b7f811c05b1d0ce
Geert van Horrik <geert@catenalogic.com>
2018-03-12 10:32:18 +0100

    Migrate to .NET SDK project style

--- src/Orc.AutomaticSupport.Tests/Orc.AutomaticSupport.Tests.Shared/Orc.AutomaticSupport.NET46.approved.cs
+++ src/Orc.AutomaticSupport.Tests/Orc.AutomaticSupport.Tests.Shared/Orc.AutomaticSupport.NET46.approved.cs
@@ -1,15 +1,12 @@
 ﻿[assembly: System.Resources.NeutralResourcesLanguageAttribute("en-US")]
 [assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
 [assembly: System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.6", FrameworkDisplayName=".NET Framework 4.6")]
-
-
 public class static ModuleInitializer
 {
     public static void Initialize() { }
 }
 namespace Orc.AutomaticSupport
 {
-    
     public class AutomaticSupportService : Orc.AutomaticSupport.IAutomaticSupportService
     {
         public AutomaticSupportService(Catel.Services.IProcessService processService, Catel.Services.IDispatcherService dispatcherService) { }
@@ -38,7 +35,6 @@ namespace Orc.AutomaticSupport
 }
 namespace Orc.AutomaticSupport.ViewModels
 {
-    
     public class RequestSupportViewModel : Catel.MVVM.ViewModelBase
     {
         public static readonly Catel.Data.PropertyData ProgressProperty;
@@ -52,7 +48,6 @@ namespace Orc.AutomaticSupport.ViewModels
 }
 namespace Orc.AutomaticSupport.Views
 {
-    
     public class RequestSupportView : Catel.Windows.Window, System.Windows.Markup.IComponentConnector
     {
         public RequestSupportView() { }
---
+++ src/Orc.AutomaticSupport/Services/EventArgs/ProgressChangedEventArgs.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProgressChangedEventArgs.cs" company="WildGums">
+//   Copyright (c) 2008 - 2014 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.AutomaticSupport
+{
+    using System;
+
+    /// <summary>
+    /// Event args for when the download progress changes.
+    /// </summary>
+    public class ProgressChangedEventArgs : EventArgs
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgressChangedEventArgs"/> class.
+        /// </summary>
+        /// <param name="progress">The progress.</param>
+        /// <param name="remainingTime">The remaining time.</param>
+        public ProgressChangedEventArgs(int progress, TimeSpan remainingTime)
+        {
+            Progress = progress;
+            RemainingTime = remainingTime;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the progress.
+        /// </summary>
+        /// <value>The progress.</value>
+        public int Progress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the remaining time.
+        /// </summary>
+        /// <value>The remaining time.</value>
+        public TimeSpan RemainingTime { get; set; }
+        #endregion
+    }
+}
