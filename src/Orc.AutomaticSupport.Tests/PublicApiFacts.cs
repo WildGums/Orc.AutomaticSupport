4cf7643ac11677f394b3333b36667c1b4a7ab2f0
Geert van Horrik <geert@catenalogic.com>
2018-04-30 14:50:25 +0200

    Fix unit tests

--- src/Orc.AutomaticSupport.Tests/PublicApiFacts.cs
+++ src/Orc.AutomaticSupport.Tests/PublicApiFacts.cs
@@ -7,13 +7,14 @@
 
 namespace Orc.AutomaticSupport.Tests
 {
+    using System.Runtime.CompilerServices;
     using ApiApprover;
     using NUnit.Framework;
 
     [TestFixture]
     public class PublicApiFacts
     {
-        [Test]
+        [Test, MethodImpl(MethodImplOptions.NoInlining)]
         public void Orc_AutomaticSupport_HasNoBreakingChanges()
         {
             var assembly = typeof(AutomaticSupportService).Assembly;
