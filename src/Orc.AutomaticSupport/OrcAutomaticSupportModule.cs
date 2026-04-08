a0793662af8ca20c27b2df720b4438ac649d5de5
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:25:09 +0200

    Convert to file scoped namespaces

--- src/Orc.AutomaticSupport/OrcAutomaticSupportModule.cs
+++ src/Orc.AutomaticSupport/OrcAutomaticSupportModule.cs
@@ -1,25 +1,24 @@
-﻿namespace Orc
+﻿namespace Orc;
+
+using Catel.Services;
+using Catel.ThirdPartyNotices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Orc.AutomaticSupport;
+
+/// <summary>
+/// Core module which allows the registration of default services in the service collection.
+/// </summary>
+public static class OrcAutomaticSupportModule
 {
-    using Catel.Services;
-    using Catel.ThirdPartyNotices;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Extensions;
-    using Orc.AutomaticSupport;
-
-    /// <summary>
-    /// Core module which allows the registration of default services in the service collection.
-    /// </summary>
-    public static class OrcAutomaticSupportModule
+    public static IServiceCollection AddOrcAutomaticSupport(this IServiceCollection serviceCollection)
     {
-        public static IServiceCollection AddOrcAutomaticSupport(this IServiceCollection serviceCollection)
-        {
-            serviceCollection.TryAddSingleton<IAutomaticSupportService, AutomaticSupportService>();
+        serviceCollection.TryAddSingleton<IAutomaticSupportService, AutomaticSupportService>();
 
-            serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.AutomaticSupport", "Orc.AutomaticSupport.Properties", "Resources"));
+        serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.AutomaticSupport", "Orc.AutomaticSupport.Properties", "Resources"));
 
-            serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.AutomaticSupport", "https://github.com/wildgums/orc.automaticsupport"));
+        serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.AutomaticSupport", "https://github.com/wildgums/orc.automaticsupport"));
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
